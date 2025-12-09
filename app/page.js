b654fae9522a12c4e32c6feebeb92dc329e6307d
Adnanasgharr <adnanasghar912@gmail.com>
2025-12-09 13:44:25 +0500

    resove issue

--- app/page.js
+++ app/page.js
@@ -2,12 +2,17 @@ import { getResources } from "@/lib/contentful";
 import ResourceTabs from "@/components/ResourceTabs";
 import Home from "@/components/Home";
 
+// 🔹 Force dynamic server-side rendering
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function ResourcesPage() {
+  // Fetch fresh resources every request
   const resources = await getResources();
 
   return (
     <main>
-      <Home/>
+      <Home />
       <ResourceTabs resources={resources} />
     </main>
   );
